0f59551af06136a2d354b804b26d63ebc47e7dc1
Dmtrs Flaco Meng <tydeas.dr@gmail.com>
2022-06-06 23:53:42 +0300

    feat(patterns/decimal): Add weakref for memory

--- patterns/flyweight/decimal.py
+++ patterns/flyweight/decimal.py
@@ -1,24 +1,22 @@
 from decimal import Decimal as PrimitiveDecimal
+from weakref import WeakValueDictionary
 
 from collections import defaultdict
 from typing import Dict
 
 from . import Flyweight
 
-# TODO: look into https://docs.python.org/3/library/weakref.html
 
 # move from PrimitiveDecimal -> T and Decimal to Flyweight
 class Decimal(PrimitiveDecimal, Flyweight):
-    _memory: Dict[int, Flyweight] = defaultdict(lambda:{})
+    _memory: WeakValueDictionary[int, Flyweight] = WeakValueDictionary()
+    
 
     # naive implementation
     def __new__(cls, *args, **kwargs) -> Flyweight:
         value = super(Decimal, cls).__new__(cls,*args, **kwargs)
         key = hash(value)
-        if key not in cls._memory:
-            cls._memory[key] = value
-        else:
-            del value
+        cls._memory[key] = cls._memory.get(key, value)
         return cls._memory[key]
 
 __all__ = [ 'Decimal' ]
